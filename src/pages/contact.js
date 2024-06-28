8cfb8cb3f10e097f44def86aa6857b98e8660a97
CANSU CANER <CANANCANSUCANER@gmail.com>
2024-06-28 18:37:26 +0200

    dependency check

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,3 +4,17 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "../../public/Components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
+
+export default function Contact() {
+  return (
+    <div>
+      <Head>
+        <title> oiloʇɟᴙoq </title>
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <Navbar />
+        <h2>Contact</h2>
+      </main>
+    </div>
+  );
+}
